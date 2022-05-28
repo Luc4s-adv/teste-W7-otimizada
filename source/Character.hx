d8a67bcaa615d7ceb297a8fb6415b31d25615219
Firey/Lucas Adv <85996884+Luc4s-adv@users.noreply.github.com>
2022-05-28 11:07:37 -0300

    Add files via upload

--- source/Character.hx
+++ source/Character.hx
@@ -94,6 +94,42 @@ class Character extends FlxSprite
 		var library:String = null;
 		switch (curCharacter)
 		{
+		case 'gf':
+		// GIRLFRIEND CODE
+		frames = AtlasFrameMaker.construct('GF_Assets_20',
+				['GF Dancing Beat','gf sad','GF Cheer','GF left note','GF Down Note','GF Up Note','GF Right Note','GF FEAR']
+				);
+				// tex = TextureUtil.downsize2(Paths.image('GF_assets'), Paths.file('images/GF_assets.xml'));
+				animation.addByPrefix('cheer', 'GF Cheer', 24, false);
+				animation.addByPrefix('thatshowyoudoit', 'GF thats how you do it', 24, false);//by Mark.
+				animation.addByPrefix('singLEFT', 'GF left note', 24, false);
+				animation.addByPrefix('singRIGHT', 'GF Right Note', 24, false);
+				animation.addByPrefix('singUP', 'GF Up Note', 24, false);
+				animation.addByPrefix('singDOWN', 'GF Down Note', 24, false);
+				animation.addByIndices('sad', 'gf sad', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], "", 24, false);
+				animation.addByIndices('danceLeft', 'GF Dancing Beat', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
+				animation.addByIndices('danceRight', 'GF Dancing Beat', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
+				animation.addByIndices('hairBlow', "GF Dancing Beat Hair blowing", [0, 1, 2, 3], "", 24);
+				animation.addByIndices('hairFall', "GF Dancing Beat Hair Landing", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], "", 24, false);
+				animation.addByPrefix('scared', 'GF FEAR', 24);
+
+				addOffset('cheer');
+				addOffset('thatshowyoudoit');
+				addOffset('sad', -2, -2);
+				addOffset('danceLeft', 0, -9);
+				addOffset('danceRight', 0, -9);
+
+				addOffset("singUP", 0, 4);
+				addOffset("singRIGHT", 0, -20);
+				addOffset("singLEFT", 0, -19);
+				addOffset("singDOWN", 0, -20);
+				addOffset('hairBlow', 45, -8);
+				addOffset('hairFall', 0, -9);
+
+				addOffset('scared', -2, -17);
+
+				playAnim('danceRight');
+
 			//case 'your character name in case you want to hardcode them instead':
 
 			default:
